40102c09e29bb8ca6c309f3a17b7e7293fb9ded8
yangzhang <yangzhang@yangzhangdeMBP.lan>
2018-04-27 16:36:31 +0800

    fix merge sort

--- src/mergeSort.js
+++ src/mergeSort.js
@@ -1,5 +1,5 @@
 function mergeSort(arr) {
-  if(arr.length === 1) return arr
+  if(arr.length <= 1) return arr
   const middleIndex = Math.floor(arr.length/2)
   const left = arr.slice(0, middleIndex)
   const right = arr.slice(middleIndex)
