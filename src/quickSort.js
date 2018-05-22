e8eb27bfa671a1651d3f5eedaf4cbd06073fc3b5
yangzhang <whu12yz@163.com>
2018-05-22 23:53:23 +0800

    fix indent

--- src/quickSort.js
+++ src/quickSort.js
@@ -1,44 +1,46 @@
-function quick(array, left, right) { let index; if (array.length > 1) {
-  index = partition(array, left, right);
-  if (left < index - 1) {
-    quick(array, left, index - 1);
+function quick(array, left, right) {
+  let index;
+  if (array.length > 1) {
+    index = partition(array, left, right);
+    if (left < index - 1) {
+      quick(array, left, index - 1);
+    }
+    if (index < right) {
+      quick(array, index, right);
+    }
   }
-  if (index < right) {
-    quick(array, index, right);
-  }
-}
-return array;
+  return array;
 }
 
 function quickSort(array) {
-return quick(array, 0, array.length - 1);
+  return quick(array, 0, array.length - 1);
 };
 
 // 划分操作函数
 function partition(array, left, right) {
-// 用index取中间值而非splice
-const pivot = array[Math.floor((right + left) / 2)];
-let i = left;
-let j = right;
+  // 用index取中间值而非splice
+  const pivot = array[Math.floor((right + left) / 2)];
+  let i = left;
+  let j = right;
 
-while (i <= j) {
-  while (compare(array[i], pivot) === -1) {
-    i++;
+  while (i <= j) {
+    while (compare(array[i], pivot) === -1) {
+      i++;
+    }
+    while (compare(array[j], pivot) === 1) {
+      j--;
+    }
+    if (i <= j) {
+      swap(array, i, j);
+      i++;
+      j--;
+    }
   }
-  while (compare(array[j], pivot) === 1) {
-    j--;
-  }
-  if (i <= j) {
-    swap(array, i, j);
-    i++;
-    j--;
-  }
-}
-return i;
+  return i;
 }
 // 原地交换函数，而非用临时数组
 function swap(array, a, b) {
-[array[a], array[b]] = [array[b], array[a]];
+  [array[a], array[b]] = [array[b], array[a]];
 }
 
 // 比较函数
@@ -46,7 +48,7 @@ function compare(a, b) {
   if (a === b) {
     return 0;
   }
-return a < b ? -1 : 1;
+  return a < b ? -1 : 1;
 }
 
-const a = quickSort([1234,1,5,19,5,2,9])
+const a = quickSort([1234, 1, 5, 19, 5, 2, 9])
