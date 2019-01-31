04cf99e2a6cde48938bf17682776fa5f139f6035
yangzhang <whu12yz@163.com>
2019-01-31 17:22:56 +0800

    lc90

--- src/lc90.js
+++ src/lc90.js
@@ -1,16 +1,14 @@
 /**
-// 输入: nums = [1,2,3]
-// 输出:
-// [
-//   [3],
-//   [1],
-//   [2],
-//   [1,2,3],
-//   [1,3],
-//   [2,3],
-//   [1,2],
-//   []
-// ]
+ * 输入: [1,2,2]
+输出:
+[
+  [2],
+  [1],
+  [1,2,2],
+  [2,2],
+  [1,2],
+  []
+]
  * @param {number[]} nums
  * @return {number[][]}
  */
@@ -31,7 +29,8 @@ function combine(nums, k) {
 
 function helper(start, nums, k, temp) {
     if (temp.length == k) {
-        result.push([].concat(temp));
+        console.log(temp);
+        result.push(JSON.stringify([].concat(temp).sort()));
     } else {
         for (let i = start; i <= nums.length; i++) {
             temp.push(nums[i - 1]);
@@ -41,4 +40,13 @@ function helper(start, nums, k, temp) {
     }
 }
 
-module.exports = subsets;
+var subsetsWithDup = function(nums) {
+    let result = new Set(subsets(nums));
+    return Array.from(result).map(item => JSON.parse(item));
+};
+
+console.log(subsetsWithDup([4, 1, 0]));
+/**
+ * [[],[1],[1,4],[1,4,4],[1,4,4,4],[1,4,4,4,4],[4],[4,4],[4,4,4],[4,4,4,4]]
+ * [[],[0],[0,1],[0,1,4],[0,4],[1],[1,4],[4]]
+ */
