c3f7e8f41b37dddaccafcdc86f6890f266ae7fae
yangzhang <whu12yz@163.com>
2019-05-26 22:10:49 +0800

    feat: 103

--- 102.二叉树的层次遍历.js
+++ 102.二叉树的层次遍历.js
@@ -14,49 +14,36 @@
  * @param {TreeNode} root
  * @return {number[][]}
  */
-// 此为DFS方式实现层次遍历, 
-// var levelOrder = function(root) {
-//     let count = 0;
-//     const res = [];
-//     if (root && root.val !== null) {
-//         res[0] = [root];
-//         count += 1
-//     }
-//     var search = function(root, res, count) {
-//         if(root && root.left) {
-//             res[count] = (res[count] || []).concat(root.left);
-//             search(root.left, res, count + 1);
-//         }
-//         if (root && root.right) {
-//             res[count] = (res[count] || []).concat(root.right);
-//             search(root.right, res, count + 1);
-//         }
-//     }
-//     search(root, res, count);
-//     return res.map((item) => {
-//         return item.map(inner => {
-//             return inner.val;
-//         });
-//     })
-// };
-
-// 此为BFS实现层次遍历
-var levelOrder = function(root) {
-    let res = [];
-    let queen = [[root, 0]];
-    while(queen.length) {
-        const task = queen.shift();
-        const node = task[0];
-        const depth = task[1];
-        if (node) {
-            res[depth] = (res[depth] || []).concat(node.val);
-        }
-        if (node && node.left) {
-            queen.push([node.left, depth + 1]);
-        }
-        if (node && node.right) {
-            queen.push([node.right, depth + 1]);
-        }
+// 此为DFS方式实现层次遍历, 方式比较粗糙，103的实现更佳
+var levelOrder = function(root, res = [], count = 0) {
+    if (!root) return []
+    res[count] = (res[count] || []).concat(root.val);
+    if(root && root.left) {
+        levelOrder(root.left, res, count + 1);
+    }
+    if (root && root.right) {
+        levelOrder(root.right, res, count + 1);
     }
     return res;
-}
+};
+
+// 此为BFS实现层次遍历
+// var levelOrder = function(root) {
+//     let res = [];
+//     let queen = [[root, 0]];
+//     while(queen.length) {
+//         const task = queen.shift();
+//         const node = task[0];
+//         const depth = task[1];
+//         if (node) {
+//             res[depth] = (res[depth] || []).concat(node.val);
+//         }
+//         if (node && node.left) {
+//             queen.push([node.left, depth + 1]);
+//         }
+//         if (node && node.right) {
+//             queen.push([node.right, depth + 1]);
+//         }
+//     }
+//     return res;
+// }
--- 103.二叉树的锯齿形层次遍历.js
+++ 103.二叉树的锯齿形层次遍历.js
@@ -14,8 +14,38 @@
  * @param {TreeNode} root
  * @return {number[][]}
  */
-var zigzagLevelOrder = function(root) {
-    
-    
-};
+// 拙劣的写法，广度优先遍历之后操作了数组
+// var zigzagLevelOrder = function(root) {
+//     let res = [];
+//     let queen = [[root, 0]];
+//     while (queen.length) {
+//         let task = queen.shift();
+//         let node  = task[0];
+//         let depth = task[1];
+//         if (node) {
+//             res[depth] = (res[depth] || []).concat(node.val);
+//         }
+//         if (node && node.left) {
+//             queen.push([node.left, depth + 1]);
+//         }
+//         if (node && node.right) {
+//             queen.push([node.right, depth + 1]);
+//         }
+//     }
+//     return res.map((item, index) => {
+//             if ((index % 2) == 0) {
+//                 return item;
+//             }
+//             return item.reverse();
+//     })
+// };
+
+// DFS的方法
+var zigzagLevelOrder = function(root, res = [], level = 0) {
+    if (!root) return [];
+    res[level] = level % 2 ? [root.val, ...res[level] || []] : [...res[level] || [], root.val];
+    root.left && zigzagLevelOrder(root.left, res, level + 1);
+    root.right && zigzagLevelOrder(root.right, res, level + 1);
+    return res;
+}
 
