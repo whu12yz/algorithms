fb0d97ed5d6d93cb9ba5226f97e71e1b2bcf4a00
yangzhang <whu12yz@163.com>
2019-05-29 22:13:34 +0800

    feat: 105

--- 105.从前序与中序遍历序列构造二叉树.js
+++ 105.从前序与中序遍历序列构造二叉树.js
@@ -16,6 +16,17 @@
  * @return {TreeNode}
  */
 var buildTree = function(preorder, inorder) {
-    
+    p = i = 0
+    build = function(stop) {
+        if (inorder[i] != stop) {
+            var root = new TreeNode(preorder[p++])
+            root.left = build(root.val);
+            i++
+            root.right = build(stop);
+            return root;
+        }
+        return null;
+    }
+    return build();
 };
 
