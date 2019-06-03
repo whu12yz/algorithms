fcfc4e40a4b1336770380a6bc8f60cd1dfaef4ee
yangzhang <whu12yz@163.com>
2019-06-03 22:11:57 +0800

    113

---
+++ 114.二叉树展开为链表.js
@@ -0,0 +1,20 @@
+/*
+ * @lc app=leetcode.cn id=114 lang=javascript
+ *
+ * [114] 二叉树展开为链表
+ */
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {void} Do not return anything, modify root in-place instead.
+ */
+var flatten = function(root) {
+    
+};
+
