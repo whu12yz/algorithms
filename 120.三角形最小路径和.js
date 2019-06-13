baa6bbb0eb9fb8c8dc53a25b26692e68b4b4344b
yangzhang <whu12yz@163.com>
2019-06-13 22:49:44 +0800

    120

---
+++ 120.三角形最小路径和.js
@@ -0,0 +1,13 @@
+/*
+ * @lc app=leetcode.cn id=120 lang=javascript
+ *
+ * [120] 三角形最小路径和
+ */
+/**
+ * @param {number[][]} triangle
+ * @return {number}
+ */
+var minimumTotal = function(triangle) {
+    
+};
+
