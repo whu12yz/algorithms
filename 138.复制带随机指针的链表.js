425b4ec10103d7f3d613f704407b0eb99a199a28
yangzhang <whu12yz@163.com>
2019-06-26 22:24:47 +0800

    138

---
+++ 138.复制带随机指针的链表.js
@@ -0,0 +1,21 @@
+/*
+ * @lc app=leetcode.cn id=138 lang=javascript
+ *
+ * [138] 复制带随机指针的链表
+ */
+/**
+ * // Definition for a Node.
+ * function Node(val,next,random) {
+ *    this.val = val;
+ *    this.next = next;
+ *    this.random = random;
+ * };
+ */
+/**
+ * @param {Node} head
+ * @return {Node}
+ */
+var copyRandomList = function(head) {
+    
+};
+
