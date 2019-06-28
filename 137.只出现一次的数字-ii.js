db78a57ee6295132a3d569bd85421a6324da7bc8
yangzhang <whu12yz@163.com>
2019-06-28 20:00:44 +0800

    137

--- 137.只出现一次的数字-ii.js
+++ 137.只出现一次的数字-ii.js
@@ -8,7 +8,10 @@
  * @return {number}
  */
 // 位运算，状态机
+function sum(list) {
+    return list.reduce((a, b) => a + b);
+}
 var singleNumber = function(nums) {
-    
+    return (sum(Array.from(new Set(nums))) * 3 - sum(nums)) / 2;
 };
 
