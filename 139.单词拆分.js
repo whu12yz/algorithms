cd3cbaae48db00bd9e9e7810f4bac8612277d77a
yangzhang <whu12yz@163.com>
2019-07-01 21:07:22 +0800

    139

--- 139.单词拆分.js
+++ 139.单词拆分.js
@@ -8,16 +8,17 @@
  * @param {string[]} wordDict
  * @return {boolean}
  */
+// 回溯法解题
 var wordBreak = function(s, wordDict) {
 
     const helper = function(index, s, wordDict) {
+        if (index === s.length) {
+            return true;
+        }
         for (let i = index; i < s.length; i ++) {
-            const tmp = s.slice(index, i + 1);
-            if (wordDict.indexOf(tmp) > -1) {
-                if (i === s.length - 1) {
+            const tmp = s.substring(index, i + 1);
+            if (wordDict.includes(tmp) && helper(i + 1, s, wordDict)) {
                     return true;
-                }
-                return helper(i + 1, s, wordDict);
             }
         }
         return false;
@@ -25,5 +26,7 @@ var wordBreak = function(s, wordDict) {
 
     return helper(0, s, wordDict);
 };
+
+// 动态规划解题
 
 
---
+++ 142.环形链表-ii.js
@@ -0,0 +1,21 @@
+/*
+ * @lc app=leetcode.cn id=142 lang=javascript
+ *
+ * [142] 环形链表 II
+ */
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var detectCycle = function(head) {
+    
+};
+
