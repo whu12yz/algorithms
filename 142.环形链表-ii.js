43682a12ffa87c07d55bd06702775c320c8753fe
yangzhang <whu12yz@163.com>
2019-07-02 22:17:47 +0800

    142

--- 142.环形链表-ii.js
+++ 142.环形链表-ii.js
@@ -16,6 +16,17 @@
  * @return {ListNode}
  */
 var detectCycle = function(head) {
-    
+    const helper = function(node, con = new WeakMap()) {
+        if (con.has(node)) return con.get(node);
+        if (!node) return null;
+
+        con.set(node, node);
+        while(node.next) {
+            return helper(node.next, con);
+        }
+        return null;
+    }
+    return helper(head)
+
 };
 
