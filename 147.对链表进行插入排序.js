3d476d948c99bfc537fc169c8fec2cbf989e64ee
yangzhang <whu12yz@163.com>
2019-07-07 22:00:29 +0800

    147

--- 147.对链表进行插入排序.js
+++ 147.对链表进行插入排序.js
@@ -15,6 +15,37 @@
  * @return {ListNode}
  */
 var insertionSortList = function(head) {
-    
+    if (!head) return null;
+
+    let newHead = head;
+    let start = newHead.next;
+    newHead.next = null;
+
+    while(start) {
+        let currentPointer = newHead;
+        if (start.val <= currentPointer.val) {
+            const newNode = new ListNode(start.val);
+            newNode.next = newHead;
+            newHead = newNode;
+        }
+
+        while (currentPointer && (start.val > currentPointer.val)) {
+            const prevPointer = currentPointer;
+            currentPointer = currentPointer.next;
+            if (currentPointer && (start.val <= currentPointer.val)) {
+                const newNode = new ListNode(start.val);
+                prevPointer.next = newNode;
+                newNode.next = currentPointer;
+                break;
+            }
+            if (!currentPointer) {
+                prevPointer.next = new ListNode(start.val);
+            }
+        }
+
+        start = start.next;
+    }
+
+    return newHead;
 };
 
