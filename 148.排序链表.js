4003b10b31519b6719736d063812226017b4c1f1
yangzhang <whu12yz@163.com>
2019-07-08 22:19:52 +0800

    148

---
+++ 148.排序链表.js
@@ -0,0 +1,20 @@
+/*
+ * @lc app=leetcode.cn id=148 lang=javascript
+ *
+ * [148] 排序链表
+ */
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var sortList = function(head) {
+    
+};
+
