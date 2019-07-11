b4c5aba40ec474536aa16425fc66808b06c3f75d
yangzhang <whu12yz@163.com>
2019-07-11 22:13:14 +0800

    152

---
+++ 152.乘积最大子序列.js
@@ -0,0 +1,13 @@
+/*
+ * @lc app=leetcode.cn id=152 lang=javascript
+ *
+ * [152] 乘积最大子序列
+ */
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxProduct = function(nums) {
+    
+};
+
