ad8076681cc7fd442461725ec6ad6f5f573e7418
yangzhang <whu12yz@163.com>
2019-07-14 21:27:38 +0800

    150detail

--- 150.逆波兰表达式求值.js
+++ 150.逆波兰表达式求值.js
@@ -7,7 +7,32 @@
  * @param {string[]} tokens
  * @return {number}
  */
+function isOperator(str) {
+    return '+-*/'.indexOf(str) > -1;
+}
+
+function calculate(operator, left, right) {
+    switch(operator) {
+        case '+': return left + right;
+        case '-': return left - right;
+        case '*': return left * right;
+        case '/': return parseInt((left / right), 10);
+        default: return null;
+    }
+}
+
 var evalRPN = function(tokens) {
-    
+    const stack = [];
+    for( let i = 0; i < tokens.length; i++) {
+        
+        if (isOperator(tokens[i])) {
+            const right = stack.pop();
+            const left = stack.pop();
+            stack.push(calculate(tokens[i], parseInt(left, 10), parseInt(right, 10)));
+        } else {
+            stack.push(tokens[i]);
+        }
+    }
+    return stack[0];
 };
 
