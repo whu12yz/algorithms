3862fd3ca1139312f23e2dd699ee460faef34f72
yangzhang <whu12yz@163.com>
2019-07-16 22:03:21 +0800

    152

--- 152.乘积最大子序列.js
+++ 152.乘积最大子序列.js
@@ -8,6 +8,18 @@
  * @return {number}
  */
 var maxProduct = function(nums) {
-    
+    if (!nums.length) return 0;
+    let max = nums[0];
+    let min = nums[0];
+    let result = max;
+    for (let i = 1; i < nums.length; i ++) {
+        let temp = max;
+        max = Math.max(Math.max(max * nums[i], min * nums[i]), nums[i]);
+        min = Math.min(Math.min(temp * nums[i], min * nums[i]), nums[i]);
+        if (max > result) {
+            result = max;
+        }
+    }
+    return result;
 };
 
