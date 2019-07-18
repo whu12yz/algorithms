14a1ed695199e7e959b3d0cd874a8997f9e31686
yangzhang <whu12yz@163.com>
2019-07-18 22:27:57 +0800

    162

--- 162.寻找峰值.js
+++ 162.寻找峰值.js
@@ -8,6 +8,29 @@
  * @return {number}
  */
 var findPeakElement = function(nums) {
-    
+
+    nums.unshift(-Infinity);
+    nums.push(-Infinity);
+
+    for (let i = 1; i < nums.length - 1; i ++) {
+        if (nums[i] > nums[i - 1] && nums[i] > nums[i + 1]) {
+            return i - 1;
+        }
+    }
 };
+
+// O(logN)
+findPeakElement = function(nums) {
+    let l = 0
+    let r = nums.length - 1;
+    while(l < r) {
+        mid = Math.floor((l + r) / 2);
+        if (nums[mid] <= nums[mid + 1]) {
+            l = mid + 1;
+        } else {
+            r = mid;
+        }
+    }
+    return l;
+}
 
---
+++ 165.比较版本号.js
@@ -0,0 +1,14 @@
+/*
+ * @lc app=leetcode.cn id=165 lang=javascript
+ *
+ * [165] 比较版本号
+ */
+/**
+ * @param {string} version1
+ * @param {string} version2
+ * @return {number}
+ */
+var compareVersion = function(version1, version2) {
+    
+};
+
