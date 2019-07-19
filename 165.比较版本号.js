3c79ac0ad8b80622759cd277cf59ea8d82195c6b
yangzhang <whu12yz@163.com>
2019-07-19 10:43:09 +0800

    165

--- 165.比较版本号.js
+++ 165.比较版本号.js
@@ -9,6 +9,19 @@
  * @return {number}
  */
 var compareVersion = function(version1, version2) {
-    
+    const v1 = version1.split('.');
+    const v2 = version2.split('.');
+    let i = 0;
+    let result = 0;
+    while (i < v1.length || i < v2.length) {
+        let v1t = parseInt(v1[i], 10) || 0;
+        let v2t = parseInt(v2[i], 10) || 0;
+        if (v1t !== v2t) {
+            result = v1t > v2t ? 1 : -1;
+            break;
+        }
+        i++;
+    }
+    return result;
 };
 
---
+++ 166.分数到小数.js
@@ -0,0 +1,14 @@
+/*
+ * @lc app=leetcode.cn id=166 lang=javascript
+ *
+ * [166] 分数到小数
+ */
+/**
+ * @param {number} numerator
+ * @param {number} denominator
+ * @return {string}
+ */
+var fractionToDecimal = function(numerator, denominator) {
+    
+};
+
