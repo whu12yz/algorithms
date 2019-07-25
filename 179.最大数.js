868176769b6b2d33ec405d2ec85fc3e3955ad6ca
yangzhang <whu12yz@163.com>
2019-07-25 21:05:57 +0800

    179

---
+++ 179.最大数.js
@@ -0,0 +1,14 @@
+/*
+ * @lc app=leetcode.cn id=179 lang=javascript
+ *
+ * [179] 最大数
+ */
+/**
+ * @param {number[]} nums
+ * @return {string}
+ */
+var largestNumber = function(nums) {
+    var sorted = [];
+    
+};
+
