287f41b497cf8e87495ec1acc70fd309fa2d6e6f
yangzhang <whu12yz@163.com>
2019-07-29 21:42:47 +0800

    183

--- 184.部门工资最高的员工.sql
+++ 184.部门工资最高的员工.sql
@@ -6,3 +6,10 @@
 # Write your MySQL query statement below
 
 
+select d.Name Department ,e1.Name Employee,e1.Salary Salary 
+from Employee e1
+inner join Department d on e1.DepartmentId= d.Id
+where e1.Salary in (select Max(e2.Salary) from Employee e2 where e2.DepartmentId =e1.DepartmentId)
+
+
+
