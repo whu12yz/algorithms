b4ab40025c6e9c2b45c13b9fd8d1ed2289a35623
yangzhang <whu12yz@163.com>
2019-08-04 20:55:58 +0800

    200

--- 200.岛屿的个数.js
+++ 200.岛屿的个数.js
@@ -7,13 +7,27 @@
  * @param {character[][]} grid
  * @return {number}
  */
+// 图结构， DFS实现
 var numIslands = function(grid) {
-    grid.forEach((value, i) => {
-        value.forEach((value, j) => {
-            if (value === 1) {
-                grid[i][j]
+    let count = 0;
+    var dfs = function(grid, i, j) {
+        if (grid[i] && grid[i][j] === '1') {
+            grid[i][j] = '0';
+            dfs(grid, i + 1, j);
+            dfs(grid, i - 1, j);
+            dfs(grid, i, j + 1);
+            dfs(grid, i, j - 1);
+        } 
+    }
+
+    for (let i = 0; i < grid.length; i ++) {
+        for (let j = 0; j < grid[0].length; j ++) {
+            if (grid[i][j] === '1') {
+                dfs(grid, i, j);
+                count ++;
             }
-        })
-    })
+        }
+    }
+    return count;
 };
 
