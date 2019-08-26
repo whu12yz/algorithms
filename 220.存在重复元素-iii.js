8e26fe9f764d031deeb4a4e1ab76ac3ed43c6cae
yangzhang <whu12yz@163.com>
2019-08-26 21:06:01 +0800

    220

--- 220.存在重复元素-iii.js
+++ 220.存在重复元素-iii.js
@@ -9,10 +9,27 @@
  * @param {number} t
  * @return {boolean}
  */
+function isNumber(value) {
+    return typeof value === 'number';
+}
 var containsNearbyAlmostDuplicate = function(nums, k, t) {
+    function check(nums, start, end) {
+        for (let i=start; i < end; i ++) {
+            for (let j = i + 1; j <= end; j ++) {
+                if (isNumber(nums[i]) && isNumber(nums[j]) && Math.abs(nums[i] - nums[j]) <= t) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
     for (let i = 0; i < nums.length; i ++) {
         let end = i + k;
-        
+        if (check(nums, i , end)) {
+            return true;
+        }
     }
+    return false;
 };
+containsNearbyAlmostDuplicate([2, 2], 3, 0);
 
