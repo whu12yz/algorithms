044826b7753a95d39bf79eb1c09cbe0553f48803
yangzhang <whu12yz@163.com>
2019-08-29 22:26:54 +0800

    223

---
+++ 223.矩形面积.js
@@ -0,0 +1,20 @@
+/*
+ * @lc app=leetcode.cn id=223 lang=javascript
+ *
+ * [223] 矩形面积
+ */
+/**
+ * @param {number} A
+ * @param {number} B
+ * @param {number} C
+ * @param {number} D
+ * @param {number} E
+ * @param {number} F
+ * @param {number} G
+ * @param {number} H
+ * @return {number}
+ */
+var computeArea = function(A, B, C, D, E, F, G, H) {
+    
+};
+
