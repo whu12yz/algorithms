9ea297a1fbc6bd583dbd63683022fb177765c6fa
yangzhang <whu12yz@163.com>
2019-09-12 18:58:28 +0800

    236

---
+++ 230.二叉搜索树中第k小的元素.js
@@ -0,0 +1,21 @@
+/*
+ * @lc app=leetcode.cn id=230 lang=javascript
+ *
+ * [230] 二叉搜索树中第K小的元素
+ */
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} k
+ * @return {number}
+ */
+var kthSmallest = function(root, k) {
+    
+};
+
