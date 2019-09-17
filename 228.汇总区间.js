816679eeab878ed220af0fab26bb51be631b99b6
yangzhang <whu12yz@163.com>
2019-09-17 22:30:21 +0800

    228

--- 228.汇总区间.js
+++ 228.汇总区间.js
@@ -8,6 +8,27 @@
  * @return {string[]}
  */
 var summaryRanges = function(nums) {
-    
+    const res = [];
+    let sides = [];
+    for (let i = 0; i < nums.length; i ++) {
+        if (nums[i] - nums[i - 1] !== 1) {
+            console.log(i);
+            if (sides.length === 2) {
+                let data;
+                if (sides[1] - sides[0] === 1) {
+                    data = nums[sides[0]];
+                } else {
+                    data = `${nums[sides[0]]}->${nums[sides[1] - 1]}`;
+                }
+                res.push(data)
+                sides.shift();
+                console.log(sides);
+            } else {
+                sides.push(i);
+            }
+            console.log(sides);
+        }
+    }
+    return res; 
 };
 
