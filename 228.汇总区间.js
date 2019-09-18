6ebb87adb13bbb267278d39e91f6e75039e79147
yangzhang <whu12yz@163.com>
2019-09-18 11:04:36 +0800

    228

--- 228.汇总区间.js
+++ 228.汇总区间.js
@@ -10,23 +10,19 @@
 var summaryRanges = function(nums) {
     const res = [];
     let sides = [];
-    for (let i = 0; i < nums.length; i ++) {
+    for (let i = 0; i <= nums.length; i ++) {
         if (nums[i] - nums[i - 1] !== 1) {
-            console.log(i);
+            sides.push(i);
             if (sides.length === 2) {
                 let data;
                 if (sides[1] - sides[0] === 1) {
-                    data = nums[sides[0]];
+                    data = nums[sides[0]].toString();
                 } else {
                     data = `${nums[sides[0]]}->${nums[sides[1] - 1]}`;
                 }
                 res.push(data)
                 sides.shift();
-                console.log(sides);
-            } else {
-                sides.push(i);
             }
-            console.log(sides);
         }
     }
     return res; 
