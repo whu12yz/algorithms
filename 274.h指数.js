1013aca358cde434df2043f67d0b3255f5944dbc
yangzhang <whu12yz@163.com>
2019-09-23 21:46:26 +0800

    274

---
+++ 274.h指数.js
@@ -0,0 +1,13 @@
+/*
+ * @lc app=leetcode.cn id=274 lang=javascript
+ *
+ * [274] H指数
+ */
+/**
+ * @param {number[]} citations
+ * @return {number}
+ */
+var hIndex = function(citations) {
+    
+};
+
