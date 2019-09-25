bc9eefa2ec23579553c33e8467f06fe524c22f64
yangzhang <whu12yz@163.com>
2019-09-25 22:38:56 +0800

    289

---
+++ 289.生命游戏.js
@@ -0,0 +1,13 @@
+/*
+ * @lc app=leetcode.cn id=289 lang=javascript
+ *
+ * [289] 生命游戏
+ */
+/**
+ * @param {number[][]} board
+ * @return {void} Do not return anything, modify board in-place instead.
+ */
+var gameOfLife = function(board) {
+    
+};
+
