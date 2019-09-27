96c8358d5c617db1392b77bee7389d387d80a81e
yangzhang <whu12yz@163.com>
2019-09-27 21:08:22 +0800

    287

--- 287.寻找重复数.js
+++ 287.寻找重复数.js
@@ -8,6 +8,17 @@
  * @return {number}
  */
 var findDuplicate = function(nums) {
-    
+
+    let low = 1, high = nums.length - 1;
+    while (low <= high) {
+        let mid = low + (high - low) * 0.5;
+        let cnt = 0;
+        for (let a of nums) {
+            if (a <= mid) ++cnt;
+        }
+        if (cnt <= mid) low = mid + 1;
+        else high = mid - 1;
+    }
+    return low;
 };
 
