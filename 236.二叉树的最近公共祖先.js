789ae14dd7642ffaa5b96a5565fadfecdf0d4da4
yangzhang <whu12yz@163.com>
2019-10-09 21:53:04 +0800

    236

--- 236.二叉树的最近公共祖先.js
+++ 236.二叉树的最近公共祖先.js
@@ -17,6 +17,11 @@
  * @return {TreeNode}
  */
 var lowestCommonAncestor = function(root, p, q) {
-    
+    if (!root) return null;
+    if (root.val === p.val || root.val === q.val) return root;
+    let left = lowestCommonAncestor(root.left, p, q);
+    let right = lowestCommonAncestor(root.right, p, q);
+    if (left && right) return root;
+    return left ? left : right;
 };
 
