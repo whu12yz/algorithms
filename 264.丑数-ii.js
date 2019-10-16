89e83983897996fd31891340cbf56a845834f1af
yangzhang <whu12yz@163.com>
2019-10-16 21:44:53 +0800

    264

--- 264.丑数-ii.js
+++ 264.丑数-ii.js
@@ -3,6 +3,20 @@
  *
  * [264] 丑数 II
  */
+function isUgly(num) {
+    if (num < 0) return false;
+    while (num % 2 === 0) {
+        num /= 2;
+    }
+    while (num % 3 === 0) {
+        num /= 3;
+    }
+    while (num % 5 === 0) {
+        num /= 5;
+    }
+    if (num === 1) return true;
+    return false;
+}
 /**
  * @param {number} n
  * @return {number}
