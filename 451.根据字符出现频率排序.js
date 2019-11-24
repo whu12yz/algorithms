3369bfcdeee20bbcb7c7aba83942d3a40bf5b36d
yangzhang <whu12yz@163.com>
2019-11-24 14:17:04 +0800

    451

--- 451.根据字符出现频率排序.js
+++ 451.根据字符出现频率排序.js
@@ -12,21 +12,24 @@
 var frequencySort = function(s) {
     // 统计，sort, join
     var map = {};
-    var list = [];
+    var max = 1;
     for(let i of s) {
         if (map[i]) {
             map[i]++;
         } else {
             map[i] = 1;
         }
+        max = Math.max(max, map[i]);
     }
-    for ( let j in map) {
-        list.push(j.repeat(map[j]));
+
+    // 利用桶排序的思想，分桶拼装长度相同的字符串
+    var list = new Array(max + 1).fill('');
+    for (let j in map) {
+        list[map[j]] += j.repeat(map[j]);
     }
-    list.sort((a, b) => {
-        return b.length - a.length;
-    })
-    return list.join('');
+
+    // 倒序输出
+    return list.reverse().join('');
 };
 // @lc code=end
 
