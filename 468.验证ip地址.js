294b2732e719b2f765727bbd8ba415ce6c101abb
yangzhang <whu12yz@163.com>
2019-12-02 22:24:58 +0800

    468

---
+++ 468.验证ip地址.js
@@ -0,0 +1,16 @@
+/*
+ * @lc app=leetcode.cn id=468 lang=javascript
+ *
+ * [468] 验证IP地址
+ */
+
+// @lc code=start
+/**
+ * @param {string} IP
+ * @return {string}
+ */
+var validIPAddress = function(IP) {
+    
+};
+// @lc code=end
+
