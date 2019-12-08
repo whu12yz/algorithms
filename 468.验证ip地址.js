af5f0455474668e8bae1caf5de560ce96c46b713
yangzhang <whu12yz@163.com>
2019-12-08 22:47:52 +0800

    468

--- 468.验证ip地址.js
+++ 468.验证ip地址.js
@@ -9,8 +9,47 @@
  * @param {string} IP
  * @return {string}
  */
+var isValidNumber = function(value) {
+    if(parseInt(value).toString() === value) return true;
+    return false;
+}
+var checkIPv4 = function(IP) {
+    const list = IP.split('.');
+    if (list.length !== 4) return 'Neither';
+    for (let i of list) {
+        if (!isValidNumber(i)) return 'Neither';
+        if (i > 255 || i < 0) {
+            return 'Neither';
+        }
+    }
+    return 'IPv4';
+}
+
+var checkIPv6 = function(IP) {
+    const list = IP.split(':');
+    if (list.length !== 8) return 'Neither';
+    for (let ii of list) {
+        if (ii.length === 4) {
+            continue;
+        }
+        if (ii.length === 1 && ii === '0') {
+            continue;
+        }
+        return 'Neither';
+    }
+    return 'IPv6';
+}
+
 var validIPAddress = function(IP) {
-    
+    if (IP.indexOf('.') > -1) {
+        return checkIPv4(IP);
+    }
+    if (IP.indexOf(':') > -1) {
+        return checkIPv6(IP);
+    }
+    return 'Neither';
 };
+
+validIPAddress("2001:0db8:85a3:0000:0:8A2E:0370:733a");
 // @lc code=end
 
