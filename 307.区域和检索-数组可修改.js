1dd00daa55572d5242f7cff7e8b5273b96ccbf4e
yangzhang <whu12yz@163.com>
2019-12-15 21:58:54 +0800

    307

---
+++ 307.区域和检索-数组可修改.js
@@ -0,0 +1,40 @@
+/*
+ * @lc app=leetcode.cn id=307 lang=javascript
+ *
+ * [307] 区域和检索 - 数组可修改
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ */
+var NumArray = function(nums) {
+    
+};
+
+/** 
+ * @param {number} i 
+ * @param {number} val
+ * @return {void}
+ */
+NumArray.prototype.update = function(i, val) {
+    
+};
+
+/** 
+ * @param {number} i 
+ * @param {number} j
+ * @return {number}
+ */
+NumArray.prototype.sumRange = function(i, j) {
+    
+};
+
+/** 
+ * Your NumArray object will be instantiated and called as such:
+ * var obj = new NumArray(nums)
+ * obj.update(i,val)
+ * var param_2 = obj.sumRange(i,j)
+ */
+// @lc code=end
+
