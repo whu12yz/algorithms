80ce5e0c08a9eb6e1655fdc5bb14286cffade9e8
medivhyang <medivhyang@tencent.com>
2022-07-21 20:56:12 +0800

    lc22

---
+++ 22.括号生成.js
@@ -0,0 +1,36 @@
+/*
+ * @lc app=leetcode.cn id=22 lang=javascript
+ *
+ * [22] 括号生成
+ */
+
+// @lc code=start
+/**
+ * @param {number} n
+ * @return {string[]}
+ */
+var generateParenthesis = function(n) {
+    const res = [];
+    // for (let i = 0; i < n; i ++) {
+    //     backtracking()
+    // } 
+    backtracking('', 0, 0); 
+    function backtracking(str, left, right) {
+        if (str.length === n * 2) res.push(str);
+        if (left < n) {
+            str += '(';
+            backtracking(str, left + 1, right );
+            str = str.slice(0, -1);
+
+        }
+        if (right < left) {
+            str += ')';
+            backtracking(str, left, right + 1);
+            str = str.slice(0, -1);
+        }
+    }
+
+    return res;
+};
+// @lc code=end
+
