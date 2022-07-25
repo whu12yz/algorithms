4ba27ae09efbb609c3dccce87c1d3b0e35b6699c
yangzhang <whu12yz@163.com>
2022-07-26 01:59:40 +0800

    lc33

--- 33.搜索旋转排序数组.js
+++ 33.搜索旋转排序数组.js
@@ -10,7 +10,32 @@
  * @param {number} target
  * @return {number}
  */
+// 二分查找
 var search = function(nums, target) {
+    const len = nums.length;
+    
+    let l = 0;
+    let r = len - 1;
+
+    while (l <= r) {
+        let mid = Math.floor((l + r) / 2);
+        if (nums[mid] === target) return mid;
+        if (nums[mid] < nums[len - 1] ) {
+            if (target > nums[mid] && target <= nums[r]) {
+                l = mid + 1;
+            } else {
+                r = mid - 1;
+            }
+        } else {
+            if (target >= nums[l] && target < nums[mid]) {
+                r = mid - 1;
+            } else {
+                l = mid + 1;
+            }
+        }
+    }
+
+    return -1;
     
 };
 // @lc code=end
